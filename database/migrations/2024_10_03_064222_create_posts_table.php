dfbff89f57c2b57778cce1b3ebe1e60740e7f2b9
Teguh Bagas Mardiansyah <teguhbagas2134@gmail.com>
2024-10-03 13:47:06 +0700

    chore: add posts table

--- database/migrations/2024_10_03_064222_create_posts_table.php
+++ database/migrations/2024_10_03_064222_create_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->foreignId('author_id');
+            $table->string('slug')->unique();
+            $table->text('body');
             $table->timestamps();
         });
     }
